8e3a05c4e469cb88c550ecb463bac9709e5073a2
Jasper <japeemga@gmail.com>
2025-03-19 11:15:42 +0800

    Input full name, print in pascal case

--- input_fullname_print_in_pascal_case.py
+++ input_fullname_print_in_pascal_case.py
@@ -1,4 +1,6 @@
 #input full name
     #fix casing
     #remove spaces
+full_name = input("Enter your full name: ").strip().title().replace(" ", "")
 #print full name in pascal case
+print(full_name)
