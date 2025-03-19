ee4f85cbd7a898c2731c933dd41a41f81d27ab37
Jasper <japeemga@gmail.com>
2025-03-19 11:18:47 +0800

    Input full name, print in snake case

--- input_fullname_print_in_snake_case.py
+++ input_fullname_print_in_snake_case.py
@@ -1,4 +1,6 @@
 #input fullname
     #fix casing
     #replace space with underscore
+full_name = input("Enter your full name: ").strip().lower().replace(" ", "_")
 #print in snake case
+print(full_name)
